409aa9e34570d3364dd18e902e701bbc122ba7ce
williamchavula <rumbani@gmail.com>
2025-05-01 10:24:23 -0500

    switch to sameSite: none

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -74,7 +74,7 @@ router.post(
       res.cookie("auth_token", token, {
         httpOnly: true,
         secure: config.NODE_ENV === "production",
-        sameSite: "lax",
+        sameSite: "none",
         maxAge: 24 * 60 * 60 * 1000,
       });
 
