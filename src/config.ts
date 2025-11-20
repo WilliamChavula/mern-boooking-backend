a57563ba354f536e3be584d39ef8ca626cc0ab4f
william chavula <rumbani@gmail.com>
2025-11-20 08:35:33 -0600

    fix: update NODE_ENV to include 'test' in config schema for better environment handling

--- src/config.ts
+++ src/config.ts
@@ -1,20 +1,20 @@
-import { z } from "zod";
-import "dotenv/config";
+import { z } from 'zod';
+import 'dotenv/config';
 
 const configSchema = z.object({
-  PORT: z.coerce.number(),
-  MONGODB_URI: z.string(),
-  SECRET_KEY: z.string(),
-  NODE_ENV: z.enum(["development", "production"]),
-  FRONTEND_URL: z.string().url("FRONTEND_URL must be a valid URL"),
-  CLOUDINARY_API_SECRET: z.string({
-    message: "CLOUDINARY_API_SECRET is Required",
-  }),
-  CLOUDINARY_NAME: z.string({ message: "CLOUDINARY_NAME is Required" }),
-  CLOUDINARY_API_KEY: z.string({ message: "CLOUDINARY_API_KEY is Required" }),
-  STRIPE_SECRET_KEY: z.string({ message: "STRIPE_SECRET_KEY is Required" }),
-  BACKEND_PROD_URL: z.string(),
-  BACKEND_DEV_URL: z.string(),
+    PORT: z.coerce.number(),
+    MONGODB_URI: z.string(),
+    SECRET_KEY: z.string(),
+    NODE_ENV: z.enum(['development', 'production', 'test']),
+    FRONTEND_URL: z.string().url('FRONTEND_URL must be a valid URL'),
+    CLOUDINARY_API_SECRET: z.string({
+        message: 'CLOUDINARY_API_SECRET is Required',
+    }),
+    CLOUDINARY_NAME: z.string({ message: 'CLOUDINARY_NAME is Required' }),
+    CLOUDINARY_API_KEY: z.string({ message: 'CLOUDINARY_API_KEY is Required' }),
+    STRIPE_SECRET_KEY: z.string({ message: 'STRIPE_SECRET_KEY is Required' }),
+    BACKEND_PROD_URL: z.string(),
+    BACKEND_DEV_URL: z.string(),
 });
 
 export const config = configSchema.parse(process.env);
